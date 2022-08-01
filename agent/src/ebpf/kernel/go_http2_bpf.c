88bc291324e23881cd86e7e6c99e0e700895e787
Nie RunJie <runjie@yunshan.net>
2022-08-01 05:52:59 -0400

    [eBPF] tcp seq 0
    **Phenomenon and reproduction steps**
    
    (Describe the phenomenon and scenario of the bug)
    
    **Root cause and solution**
    
    (Describe the cause of the bug and corresponding solution)
    
    **Impactions**
    
    (Visible changes after this fix)
    
    **Test method**
    
    (Test method to ensure the bug is fixed)
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- agent/src/ebpf/kernel/go_http2_bpf.c
+++ agent/src/ebpf/kernel/go_http2_bpf.c
@@ -213,6 +213,8 @@ get_response_table_from_http2clientConnReadLoop_ctx(struct pt_regs *ctx)
 	return get_http2ClientConn_response_table(ptr);
 }
 
+// 执行到这个 hook 点的时候可能还在初始化阶段,将产生 tcp_seq 序列号为 0 的值
+// TODO: 屏蔽掉为 0 tcp_seq 的数据还是上报
 static __inline __u32 get_previous_read_tcp_seq(int fd, __u32 seq_end)
 {
 	struct http2_tcp_seq_key key = {
